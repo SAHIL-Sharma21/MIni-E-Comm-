0168ab5c64746807fad3730d12467a3bfe6a1aff
Sahil Sharma <sahilsharma2000dtc@gmail.com>
2024-03-03 22:05:31 +0530

    api called in ts

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,31 +1,48 @@
 //here we are going to fetch the products from the api
 
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 
 
 const Products: React.FC = () => {
 
-    // const [data, setData] = useState<response| null>(null);
+    const [data, setData] = useState<Product[]| null>(null);
     //making api call
-interface response {
-    data: {},
+    // i wqs not able to think thiss >>>> learning no1
+interface Product {
+    category: string,
+    price: number,
+    thumbnail: string,
+    images: string[],
+    title: string,
+    id: number
 }
 
     //api call
-const fetchData = async() => {
-    try {
-        const response = await fetch('https://api.freeapi.app/api/v1/public/randomproducts?page=1&limit=10&inc=category%2Cprice%2Cthumbnail%2Cimages%2Ctitle%2Cid&query=mens-watches');
-        const data: response = await response.json();
-        console.log(data);
+// const fetchData = async() => {
+//     try {
+//         const response = await fetch('https://api.freeapi.app/api/v1/public/randomproducts?page=1&limit=10&inc=category%2Cprice%2Cthumbnail%2Cimages%2Ctitle%2Cid&query=mens-watches');
+//         const data: response = await response.json();
+//         console.log(data);
         
 
-    } catch (error) {
-        console.log(error);
-    }
-}
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
 
     useEffect(()=> {
+        const fetchData = async() => {
+            try {
+                const response = await fetch('https://api.freeapi.app/api/v1/public/randomproducts?page=1&limit=10&inc=category%2Cprice%2Cthumbnail%2Cimages%2Ctitle%2Cid&query=mens-watches');
+                const data = await response.json();
+                // console.log(data.data.data[2]); 
+                setData(data.data.data);  
+            } catch (error) {
+                console.log('Error making request:', error);
+            }
+
+        }
         fetchData();
     }, [])
 
@@ -34,7 +51,15 @@ const fetchData = async() => {
         <>
             <div>
                 <h1>Store products </h1>
-                <h2>data : {}</h2>
+                    {/* looping  through array if data is present then it will render  */}
+                    {data && data.map((prod: Product) => (
+                        <div key={prod.id}>
+                            <h2>{prod.title}</h2>
+                            <p>{prod.category}</p>
+                            <p>{prod.price}</p>
+                            <img src={prod.thumbnail} alt={prod.title} />
+                        </div>
+                    ))}
             </div>
         </>
     )
